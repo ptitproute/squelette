4de247fdca9d67272461b320761aecb5f0c45f04
gitea_admin <thibault.marchal@solyti.fr>
2023-11-08 20:26:34 +0100

    retrait de mon ip gitea boulot lol

--- github.sh
+++ github.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export GITHUB_PASSWORD=${GITHUB_PASSWORD:=$(read -p "GITEA_PASSWORD non définie, merci de saisir votre mot de passe git : " -s temporaire; echo $temporaire)}
-
 git add .
-git commit -p "update"
-git push -u https://t.marchal:$(mot_de_passe_encode_url "$GITEA_PASSWORD")@gitea.solyti.com/modelage/debian12.git master
+git commit -p
+git -c "core.sshCommand=ssh -i cle.prive.github" push -u origin master
